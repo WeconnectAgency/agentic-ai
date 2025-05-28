20010c346754b935544058d80dc2d5e27a631373
Carlos Vera <cavuforex@gmail.com>
2025-05-28 00:22:27 -0600

    fix: solucionando conflictos de react-jsx-runtime con SWC y Node 20

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,15 @@
+// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc'; 
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    assetsInlineLimit: 0, 
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]'
-      }
+    target: 'esnext' 
+  },
+  resolve: {
+    alias: {
+      'react/jsx-runtime': 'react/jsx-runtime.js' 
     }
   }
 });
