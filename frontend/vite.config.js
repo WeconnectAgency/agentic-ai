da3cc34ba18e39bf43a24291215df20bd40cc801
Carlos Vera <cavuforex@gmail.com>
2025-05-28 00:38:56 -0600

    build: corregido error de jsx-runtime con alias absoluto

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc'; 
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext' 
+    target: 'esnext'
   },
   resolve: {
     alias: {
-      'react/jsx-runtime': 'react/jsx-runtime.js' 
+      'react/jsx-runtime': path.resolve('./node_modules/react/jsx-runtime.js')
     }
   }
 });
